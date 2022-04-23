348466958192bc3d2e1d6adf2fca28f72f1d857a
logan-lieou <logan.lieou@gmail.com>
2022-04-23 03:34:06 -0500

    tracker for cock

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 export default function Home() {
   return (
-    <div>
-      <h1>COCK</h1>
+    <div style={{width: 800, margin: "auto", marginTop: 20}}>
+      <h1>Enter Your Cock Stats</h1>
         <hr/>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Cock Size</Form.Label>
+          <Form.Control type="text" placeholder="Enter cock size" />
+          <Form.Text className="text-muted">
+            We'll never share your cock size with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Girth</Form.Label>
+          <Form.Control type="text" placeholder="Enter cock girth" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Agree to share your cock with us?" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   )
 }
